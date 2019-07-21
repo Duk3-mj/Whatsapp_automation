e7355a57d7a9f169c6eee13b2faf808d822370bb
mohitrock1 <jauharimohit2@gmail.com>
2019-07-22 03:59:49 +0530

    Add files via upload

---
+++ whatsapp_automation.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com/")
+f1 = open('chat','w')
+time.sleep(15)
+
+driver.find_element_by_css_selector("._2UaNq._2ko65").click()  # to select unread chat
+time.sleep(10)
+try:
+    sachin = driver.find_elements_by_css_selector("._12pGw.EopGb")
+except NoSuchElementException:
+    pass
+for i in range(len(sachin)):
+    f1.writelines(sachin[i].text) # write chat in text file
+    f1.write('\n')
+f1.close()
+f2 = open('chat','r')
+print(f2.read())
+f2.close()
