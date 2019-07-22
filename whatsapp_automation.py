e4f19b53f18a6a32005a50e9dea836c8933f6fed
mohitrock1 <jauharimohit2@gmail.com>
2019-07-22 09:51:38 +0530

    Update whatsapp_automation.py

--- whatsapp_automation.py
+++ whatsapp_automation.py
@@ -6,17 +6,18 @@ driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com/")
 print('Please Scan the QR Code and press enter')
 input()
-f1 = open('chat','w')
-driver.find_element_by_css_selector("._2UaNq._2ko65").click()  # to select unread chat
-time.sleep(10)
 try:
-    sachin = driver.find_elements_by_css_selector("._12pGw.EopGb")
-except NoSuchElementException:
-    pass
-for i in range(17,len(sachin)):
+  f1 = open('chat','w')
+  driver.find_element_by_css_selector("._2UaNq._2ko65").click()  # to select unread chat
+  time.sleep(10)
+  sachin = driver.find_elements_by_css_selector("._12pGw.EopGb")
+  for i in range(17,len(sachin)):
     f1.writelines(sachin[i].text) # write chat in text file
     f1.write('\n')
-f1.close()
-f2 = open('chat','r')
-print(f2.read())
-f2.close()
+  f1.close()
+  f2 = open('chat','r')
+  print(f2.read())
+  f2.close()
+except NoSuchElementException:
+  print("you have no new messages")
+
